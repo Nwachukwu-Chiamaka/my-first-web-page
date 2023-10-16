f0637c35f2ffa2e9097bcadd340bcf432534084c
AMMIEE <bigammy001@gmail.com>
2023-10-16 05:48:49 +0100

    first commit - committing all files to the repository

---
+++ dom.js
@@ -0,0 +1,41 @@
+
+//   function simulateButtonClick() {
+//     // Get a reference to the button element by its id
+//     const button = document.getElementsByClassName("download-butn");
+
+//     // Simulate a click on the button
+//     button.click();
+//   }
+
+// <button id="myButton">Click me</button>
+// <button id="clickButton">Simulate Click</button>
+
+
+
+// {
+
+//     function simulateButtonClick() {
+//       // Get a reference to the button element by its id
+//       const  button = document.getElementsByClassName("download-butn");
+  
+//       // Simulate a click on the button
+//       button.click();
+//     }
+//   {
+//       // Add a click event listener to the "Simulate Click" button
+//       const clickButton = document.getElementsByClassName("download-butn");
+//       clickButton.addEventListener("click", simulateButtonClick);
+//   }
+// }
+
+
+
+const button = document.getElementsByClassName("click")
+console.log('test', button);
+button.addEventListener('click', function() {
+    console.log
+})
+
+setInterval(() => {
+    
+}, 1000);
